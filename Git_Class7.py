b1a45ad4e7ca3e5d86d5ed8f816876c1182f1172
DimaBorovskydimab1143@gmail.comDimaBorovsky <dimab1143@gmail.com>
2024-08-24 16:46:06 +0300

    Space

--- Git_Class7.py
+++ Git_Class7.py
@@ -6,6 +6,10 @@ def disk_resize(disk_name, machine_name,target_gb):
     print("resizing disk")
     print("SSHing to machine")
 
+
+
+
+
 disk_name = getenv("DISK_NAME")
 machine_name = getenv("MACHINE_NAME")
 target_gb = getenv("TARGET_GB")
